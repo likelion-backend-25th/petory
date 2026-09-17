c95013f3d5abbbcb6941e0eb5226293f263980c1
00-ydh <yuhun3219@gmail.com>
2026-09-17 13:34:45 +0900

    first commit

---
+++ src/main/java/net/likelion/bebc25/projectpatory/ProjectPatoryApplication.java
@@ -0,0 +1,13 @@
+package net.likelion.bebc25.projectpatory;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectPatoryApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProjectPatoryApplication.class, args);
+    }
+
+}
---
+++ src/test/java/net/likelion/bebc25/projectpatory/ProjectPatoryApplicationTests.java
@@ -0,0 +1,13 @@
+package net.likelion.bebc25.projectpatory;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectPatoryApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

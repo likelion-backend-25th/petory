69ce8e7127543ca7f3ceca5eed24240f336c6762
Byeongho Kim <qudgh0217@naver.com>
2026-09-24 14:03:20 +0900

    fix:basicLogin401Error - 원인: SecurityConfig 설정오류

--- src/main/java/net/likelion/bebc25/projectpatory/security/config/SecurityConfig.java
+++ src/main/java/net/likelion/bebc25/projectpatory/security/config/SecurityConfig.java
@@ -66,7 +66,7 @@ public class SecurityConfig {
                         .requestMatchers("/error").permitAll()
 
                         // 인증 API (login, refresh)
-                        .requestMatchers("/api/v1/auth/**").permitAll()
+                        .requestMatchers("/api/v1/login").permitAll()
 
                         // 회원가입
                         .requestMatchers("/api/v1/signup").permitAll()
